bd807da2690a62a198e7b2fe70df8d0d3ff578d7
Sinan Mustafov <ss.mustafov@gmail.com>
2015-01-07 18:08:52 +0200

    Initial commit of the server side

---
+++ Chat-Application-Server-Side/src/main/java/com/sirma/itt/javacourse/chatserver/commands/LogoutCommand.java
@@ -0,0 +1,51 @@
+package com.sirma.itt.javacourse.chatserver.commands;
+
+import java.util.ResourceBundle;
+
+import com.sirma.itt.javacourse.chatcommon.utils.Query;
+import com.sirma.itt.javacourse.chatcommon.utils.QueryTypes;
+import com.sirma.itt.javacourse.chatserver.server.Client;
+import com.sirma.itt.javacourse.chatserver.server.ServerDispatcher;
+import com.sirma.itt.javacourse.chatserver.utils.LanguageBundleSingleton;
+import com.sirma.itt.javacourse.chatserver.views.View;
+
+/**
+ * @author Sinan
+ */
+public class LogoutCommand extends ServerCommand {
+
+	private ResourceBundle bundle = LanguageBundleSingleton.getBundleInstance();
+
+	/**
+	 * @param serverDispatcher
+	 *            - the
+	 * @param view
+	 *            - the
+	 */
+	public LogoutCommand(ServerDispatcher serverDispatcher, View view) {
+		super(serverDispatcher, view);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void execute(Client client) {
+		if (!getServerDispatcher().containsClient(client.getNickname())) {
+			// client.sendQuery(new Query(QueryTypes.Refused, "O you hacked me"));
+			return;
+		}
+
+		getServerDispatcher().removeClient(client);
+		client.sendQuery(new Query(QueryTypes.Success, "You have logged out"));
+
+		String clientDisconnectedMessage = String.format("%s%s %s", "@", client.getNickname(),
+				bundle.getString("clientDisconnected"));
+		getServerView().appendMessageToConsole(clientDisconnectedMessage);
+		getServerView().removeOnlineClient(client.getNickname());
+
+		getServerDispatcher().dispatchQuery(
+				new Query(QueryTypes.ClientDisconnected, client.getNickname()));
+	}
+
+}
---
+++ Chat-Application-Server-Side/src/main/java/com/sirma/itt/javacourse/chatserver/main/Run.java
@@ -0,0 +1,40 @@
+package com.sirma.itt.javacourse.chatserver.main;
+
+import javax.swing.SwingUtilities;
+
+import com.sirma.itt.javacourse.chatserver.views.ServerView;
+import com.sirma.itt.javacourse.chatserver.views.ViewObserver;
+
+/**
+ * Runs the server of the chat application.
+ * 
+ * @author Sinan
+ */
+public final class Run {
+
+	/**
+	 * Protects from instantiation.
+	 */
+	private Run() {
+
+	}
+
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 *            - arguments
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				ServerView view = new ServerView();
+				ViewObserver viewObserver = new ViewObserver();
+				view.addObserver(viewObserver);
+			}
+		});
+	}
+
+}
