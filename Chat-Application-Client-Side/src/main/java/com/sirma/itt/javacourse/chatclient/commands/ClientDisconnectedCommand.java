3543c718dd9c8c2d6a8776b9526a26a4c6aadc92
Sinan Mustafov <ss.mustafov@gmail.com>
2015-01-07 18:22:13 +0200

    Initial commit of the client side

---
+++ Chat-Application-Client-Side/src/main/java/com/sirma/itt/javacourse/chatclient/commands/ClientDisconnectedCommand.java
@@ -0,0 +1,29 @@
+package com.sirma.itt.javacourse.chatclient.commands;
+
+import com.sirma.itt.javacourse.chatclient.views.View;
+import com.sirma.itt.javacourse.chatcommon.utils.Query;
+
+/**
+ * @author smustafov
+ */
+public class ClientDisconnectedCommand extends ClientCommand {
+
+	private Query query;
+
+	/**
+	 * @param view
+	 */
+	public ClientDisconnectedCommand(View view, Query query) {
+		super(view);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void execute() {
+		getClientView().removeOnlineClient(query.getMessage());
+		getClientView().appendMessageToChatArea("@" + query.getMessage() + " left");
+	}
+
+}
